4a7838616ff666daff862062525b44735740f98f
ogzambrano <58596873+ogzambrano@users.noreply.github.com>
2023-01-03 16:20:45 -0400

    Add files via upload

---
+++ src/pack/Main.java
@@ -0,0 +1,31 @@
+package pack;
+
+public class Main {
+    public static void main(String[] args) {
+        boolean var0 = true;
+
+        byte var1 = 12;
+        short var2 = 1;
+        int var3 = 10;
+        long var4 = 123456;
+
+        float var5 = 456.789f;
+        double var6 = 123.456d;
+
+        char var7 = 'a';
+        String var8 = "qwerty";
+
+        System.out.println("esto es un boleano " + var0);
+        System.out.println("estos son entero");
+        System.out.println("byte\tshort\tint\tlong");
+        System.out.println(var1+"\t\t"+var2+"\t\t"+var3+"\t"+var4);
+        System.out.println("estos son decimales");
+        System.out.println("float\tdouble");
+        System.out.println(var5+"\t"+var6);
+        System.out.println("estos son texto");
+        System.out.println("char\tString");
+        System.out.println(var7+"\t\t"+var8);
+
+
+    }
+}
